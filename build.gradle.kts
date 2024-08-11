c57dcb553066ec97d457060da0cee4267074327c
주진현 <joojinhyun00@gmail.com>
2024-08-12 03:28:15 +0900

    git init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "blog.jinhyun"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
