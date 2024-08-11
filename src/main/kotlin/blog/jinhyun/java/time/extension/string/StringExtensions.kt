7188f0197d384055f943e74ff2cacaf02b6cb42d
주진현 <joojinhyun00@gmail.com>
2024-08-12 07:56:41 +0900

    feature: DateTimeFormatter를 사용하는 String to LocalDate 기능 추가

--- src/main/kotlin/blog/jinhyun/java/time/extension/string/StringExtensions.kt
+++ src/main/kotlin/blog/jinhyun/java/time/extension/string/StringExtensions.kt
@@ -35,6 +35,22 @@ fun String.toLocalDate(
 ): LocalDate = LocalDate.parse(this, format.toDateTimeFormatter())
 
 /**
+ * 문자열을 [LocalDate]로 변환합니다.
+ *
+ * ```kotlin
+ * val date: LocalDate = "2022-01-01".toLocalDate(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+ * ```
+ *
+ * @receiver 변환할 문자열
+ * @param formatter [DateTimeFormatter] 객체
+ * @return [LocalDate] 객체
+ * @throws DateTimeParseException 문자열 분석에 실패한 경우
+ */
+fun String.toLocalDate(
+    formatter: DateTimeFormatter,
+): LocalDate = LocalDate.parse(this, formatter)
+
+/**
  * 문자열을 [LocalDate]로 변환합니다. 변환에 실패하면 `null`을 반환합니다.
  *
  * ```kotlin
@@ -53,3 +69,21 @@ fun String.toLocalDateOrNull(
     null
 }
 
+/**
+ * 문자열을 [LocalDate]로 변환합니다. 변환에 실패하면 `null`을 반환합니다.
+ *
+ * ```kotlin
+ * val date: LocalDate? = "2022-01-01".toLocalDateOrNull(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+ * ```
+ *
+ * @receiver 변환할 문자열
+ * @param formatter [DateTimeFormatter] 객체
+ * @return [LocalDate] 객체
+ */
+fun String.toLocalDateOrNull(
+    formatter: DateTimeFormatter,
+): LocalDate? = try {
+    toLocalDate(formatter)
+} catch (e: Exception) {
+    null
+}
