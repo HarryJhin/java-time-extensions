b9c7733c0a2fece5acd714e91346cf23fa7e71d2
주진현 <joojinhyun00@gmail.com>
2024-08-12 06:25:02 +0900

    feature: String to LocalDate 기능 추가

---
+++ src/main/kotlin/blog/jinhyun/java/time/extension/string/StringExtensions.kt
@@ -0,0 +1,55 @@
+package blog.jinhyun.java.time.extension.string
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+
+/**
+ * 문자열을 [DateTimeFormatter]로 변환합니다.
+ *
+ * ```kotlin
+ * val formatter: DateTimeFormatter = "yyyy-MM-dd".toDateTimeFormatter()
+ * ```
+ *
+ * @receiver 변환할 문자열
+ * @return [DateTimeFormatter] 객체
+ * @throws IllegalArgumentException 날짜 포맷이 유효하지 않은 경우
+ */
+fun String.toDateTimeFormatter(): DateTimeFormatter = DateTimeFormatter.ofPattern(this)
+
+/**
+ * 문자열을 [LocalDate]로 변환합니다.
+ *
+ * ```kotlin
+ * val date: LocalDate = "2022-01-01".toLocalDate()
+ * ```
+ *
+ * @receiver 변환할 문자열
+ * @param format 날짜 형식
+ * @return [LocalDate] 객체
+ * @throws DateTimeParseException 문자열 분석에 실패한 경우
+ * @throws IllegalArgumentException 날짜 포맷이 유효하지 않은 경우
+ */
+fun String.toLocalDate(
+    format: String = "yyyy-MM-dd",
+): LocalDate = LocalDate.parse(this, format.toDateTimeFormatter())
+
+/**
+ * 문자열을 [LocalDate]로 변환합니다. 변환에 실패하면 `null`을 반환합니다.
+ *
+ * ```kotlin
+ * val date: LocalDate? = "2022-01-01".toLocalDateOrNull()
+ * ```
+ *
+ * @receiver 변환할 문자열
+ * @param format 날짜 형식
+ * @return [LocalDate] 객체
+ */
+fun String.toLocalDateOrNull(
+    format: String = "yyyy-MM-dd",
+): LocalDate? = try {
+    toLocalDate(format)
+} catch (e: Exception) {
+    null
+}
+
