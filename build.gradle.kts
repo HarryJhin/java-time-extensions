6a09af08c1719473ce61ae981823ad969e624a92
주진현 <joojinhyun00@gmail.com>
2024-08-13 05:33:33 +0900

    feature: MavenCentral publish 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,9 @@ import java.net.URL
 plugins {
     kotlin("jvm") version "2.0.0"
     id("org.jetbrains.dokka") version "1.9.20"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    `maven-publish`
+    signing
 }
 
 group = "io.github.harryjhin"
@@ -55,4 +58,64 @@ tasks.register<Jar>("dokkaJavadocJar") {
     dependsOn(tasks.dokkaJavadoc)
     from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
     archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("java-time-extensions")
+                description.set("Java Time Extensions")
+                url.set("https://github.com/HarryJhin/java-time-extensions")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("harryjhin")
+                        name.set("Harry Jhin")
+                        email.set("joojinhyun00@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Hjson/java-time-extensions.git")
+                    developerConnection.set("scm:git:ssh://github.com/Hjson/java-time-extensions.git")
+                    url.set("https://github.com/HarryJhin/java-time-extensions/tree/master")
+                }
+            }
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            artifacts {
+                artifact(tasks["dokkaJavadocJar"])
+                artifact(tasks.kotlinSourcesJar) {
+                    classifier = "sources"
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("GPG_SECRET_KEY"),
+        System.getenv("GPG_PASSWORD"),
+    )
+    sign(publishing.publications)
 }
