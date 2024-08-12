4565e83cb1ac7dc9a30a6b79fc341fc01d0c48df
주진현 <joojinhyun00@gmail.com>
2024-08-13 08:13:25 +0900

    refactor: 속성 주입 및 참조 방식 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,6 @@ import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.dokka.gradle.DokkaTask
 import java.net.URL
 
-val signingPassphrase: String by project
-val signingPublicKey: String by project
-val signingSecretKey: String by project
-val releaseGithubToken: String by project
-val deployMavenMavenCentralSonatypeUsername: String by project
-val deployMavenMavenCentralSonatypePassword: String by project
-
 plugins {
     kotlin("jvm") version "2.0.0"
     id("org.jetbrains.dokka") version "1.9.20"
@@ -145,17 +138,17 @@ jreleaser {
     signing {
         setActive("ALWAYS")
         armored = true
-        publicKey = signingPublicKey
-        secretKey = signingSecretKey
-        passphrase = signingPassphrase
+        publicKey = System.getProperty("GPG_PUBLIC_KEY")
+        secretKey = System.getProperty("GPG_SECRET_KEY")
+        passphrase = System.getProperty("GPG_PASSPHRASE")
     }
     deploy {
         maven {
             mavenCentral {
                 create("sonatype") {
                     setActive("ALWAYS")
-                    username = deployMavenMavenCentralSonatypeUsername
-                    password = deployMavenMavenCentralSonatypePassword
+                    username = System.getenv("SONATYPE_USERNAME")
+                    password = System.getenv("SONATYPE_PASSWORD")
                     url = "https://central.sonatype.com/api/v1/publisher"
                     stagingRepository("build/staging-deploy")
                 }
