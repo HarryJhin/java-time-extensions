52adbccdbe3726c75ad07972f1a3180800f385d2
주진현 <joojinhyun00@gmail.com>
2024-08-13 00:34:50 +0900

    feature: LocalDate to String 함수 추가

--- src/main/kotlin/blog/jinhyun/java/time/extension/date/JtLocalDateExtensions.kt
+++ src/main/kotlin/blog/jinhyun/java/time/extension/date/JtLocalDateExtensions.kt
@@ -1,13 +1,66 @@
 package blog.jinhyun.java.time.extension.date
 
+import blog.jinhyun.java.time.extension.JavaTimeDisplay
+import blog.jinhyun.java.time.extension.string.toDateTimeFormatter
 import java.time.LocalDate
 import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 /**
- * [LocalDate]의 연도와 월 정보를 [YearMonth]로 변환합니다.
+ * [LocalDate] 인스턴스를 [YearMonth]로 변환합니다.
  *
  * @receiver [LocalDate] 객체
  * @return [YearMonth] 객체
- * @sample blog.jinhyun.java.time.extension.date.JtLocalDateExtensionsTest.toYearMonth
  */
 fun LocalDate.toYearMonth(): YearMonth = YearMonth.of(year, month)
+
+/**
+ * [LocalDate] 인스턴스를 [pattern] 형식으로 포맷팅합니다.
+ *
+ * 예시)
+ * ```kotlin
+ * val date = LocalDate.of(2022, 1, 1)
+ * val formatted: String = date.toFormattedString("yyyy-MM-dd")
+ * ```
+ *
+ * @receiver [LocalDate] 객체
+ * @param pattern 날짜 형식
+ * @return 날짜 문자열
+ */
+fun LocalDate.toFormattedString(
+    pattern: String = "yyyy-MM-dd",
+): String = this.format(pattern.toDateTimeFormatter())
+
+/**
+ * [LocalDate] 인스턴스를 [formatter]로 포맷팅합니다.
+ *
+ * 예시)
+ * ```kotlin
+ * val date = LocalDate.of(2022, 1, 1)
+ * val formatted: String = date.toFormattedString(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+ * ```
+ *
+ * @receiver [LocalDate] 객체
+ * @param formatter [DateTimeFormatter] 객체
+ * @return 날짜 문자열
+ */
+fun LocalDate.toFormattedString(
+    formatter: DateTimeFormatter,
+): String = this.format(formatter)
+
+/**
+ * [LocalDate] 인스턴스를 [display]로 포맷팅합니다.
+ *
+ * 예시)
+ * ```kotlin
+ * val date = LocalDate.of(2022, 1, 1)
+ * val formatted: String = date.toFormattedString(JavaTimeDisplay.DEFAULT)
+ * ```
+ *
+ * @receiver [LocalDate] 객체
+ * @param display [JavaTimeDisplay] 인스턴스
+ * @return 날짜 문자열
+ */
+fun LocalDate.toFormattedString(
+    display: JavaTimeDisplay,
+): String = this.format(display.dateFormatter)
