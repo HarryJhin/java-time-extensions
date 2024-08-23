318c146b59de1aa94466da0dfdff3a13f54f1df5
주진현 <joojinhyun00@gmail.com>
2024-08-23 15:40:43 +0900

    [#1] style: String 확장 함수 지원 파일 패키지 이동

--- src/main/kotlin/io/github/harryjhin/java/time/extension/primitive/StringExtensions.kt
+++ src/main/kotlin/io/github/harryjhin/java/time/extension/primitive/StringExtensions.kt
@@ -1,4 +1,4 @@
-package io.github.harryjhin.java.time.extension.string
+package io.github.harryjhin.java.time.extension.primitive
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
