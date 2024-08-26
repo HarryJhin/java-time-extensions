a158e668c6e4b05b12e4fcccd0a95cf914a8c479
주진현 <joojinhyun00@gmail.com>
2024-08-26 09:49:49 +0900

    #15 feat: YearMonth at 중위 연산자 추가

--- src/main/kotlin/io/github/harryjhin/java/time/extension/YearMonthExtensions.kt
+++ src/main/kotlin/io/github/harryjhin/java/time/extension/YearMonthExtensions.kt
@@ -31,6 +31,21 @@ val YearMonth.years: Period
 val YearMonth.months: Period
     get() = this.monthValue.months
 
+/**
+ * [YearMonth]에 일(day)를 결합하여 [LocalDate]를 생성합니다.
+ *
+ * 이 함수는 중위 표기법을 사용하여 가독성 있게 [LocalDate]를 생성할 수 있습니다.
+ *
+ * 예시:
+ * ```kotlin
+ * val date = YearMonth.of(2022, 1) at 1 // 2022-01-01
+ * ```
+ *
+ * @param dayOfMonth 결합할 일(day)
+ * @return 생성된 [LocalDate] 인스턴스
+ * @since 0.6.0
+ * @sample io.github.harryjhin.java.time.extension.YearMonthTest.atDayOfMonth
+ */
 infix fun YearMonth.at(dayOfMonth: Int): LocalDate {
     return this.atDay(dayOfMonth)
 }
