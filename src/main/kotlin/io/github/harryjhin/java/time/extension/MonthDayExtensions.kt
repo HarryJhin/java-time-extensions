0f9977f4a1fbd3ff9406b086a8a376aad4668381
주진현 <joojinhyun00@gmail.com>
2024-08-26 13:43:32 +0900

    #16 feat: MonthDay at() 중위 연산자 추가

--- src/main/kotlin/io/github/harryjhin/java/time/extension/MonthDayExtensions.kt
+++ src/main/kotlin/io/github/harryjhin/java/time/extension/MonthDayExtensions.kt
@@ -1,7 +1,10 @@
 package io.github.harryjhin.java.time.extension
 
+import java.time.DateTimeException
+import java.time.LocalDate
 import java.time.MonthDay
 import java.time.Period
+import java.time.Year
 
 /**
  * [MonthDay]를 월(month) 단위의 [Period]로 변환합니다.
@@ -30,3 +33,41 @@ val MonthDay.months: Period
  */
 val MonthDay.days: Period
     get() = this.dayOfMonth.days
+
+/**
+ * [MonthDay]에 [Year]를 결합하여 [LocalDate]를 생성합니다.
+ *
+ * 이 함수는 중위 표기법을 사용하여 가독성 있게 [LocalDate]를 생성할 수 있습니다.
+ *
+ * 예시:
+ * ```kotlin
+ * val date: LocalDate = MonthDay.of(1, 1) at Year.of(2022) // 2022-01-01
+ * ```
+ *
+ * @param year 결합할 [Year]
+ * @return 생성된 [LocalDate] 인스턴스
+ * @throws DateTimeException 연도 값이 유효하지 않은 경우
+ * @since 0.8.0
+ */
+infix fun MonthDay.at(year: Year): LocalDate {
+    return this.atYear(year.value)
+}
+
+/**
+ * [MonthDay]에 [year]를 결합하여 [LocalDate]를 생성합니다.
+ *
+ * 이 함수는 중위 표기법을 사용하여 가독성 있게 [LocalDate]를 생성할 수 있습니다.
+ *
+ * 예시:
+ * ```kotlin
+ * val date: LocalDate = MonthDay.of(1, 1) at 2022 // 2022-01-01
+ * ```
+ *
+ * @param year 결합할 연도 값
+ * @return 생성된 [LocalDate] 인스턴스
+ * @throws DateTimeException 연도 값이 유효하지 않은 경우
+ * @since 0.8.0
+ */
+infix fun MonthDay.at(year: Int): LocalDate {
+    return this.atYear(year)
+}
